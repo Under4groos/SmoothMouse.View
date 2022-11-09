f82b9e2c3db5f807224f4fd1856456b33e3420cc
Under4groos <hhit934@gmail.com>
2022-11-09 21:13:46 +0300

    фывфыв

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    main()
+});
+
+function SetElement_onclick(str_name, str_url) {
+    var element_ = document.getElementById(str_name);
+
+    element_.onclick = function() {
+
+        window.open(str_url, '_blank');
+    }
+    return element_
+}
+
+
+
+function main() {
+
+    let git_ = new github()
+
+    git_.Get("https://api.github.com/repos/Under4groos/CustView.View/releases", (data) => {
+        SetElement_onclick("but_view_demo", data[0]["assets"][0]["browser_download_url"])
+    });
+
+    document.getElementById('image')
+        .style.display = "block";
+
+
+
+}
